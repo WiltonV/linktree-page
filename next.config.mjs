52191e32434158770579f4107c0aa560b343416e
Wilton Vieira <wlv.wilton@gmail.com>
2026-02-08 19:09:06 -0300

    Add .nojekyll file and rebuild project

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/linktree-page',
+  output: "export",
+  basePath: "/linktree-page",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
